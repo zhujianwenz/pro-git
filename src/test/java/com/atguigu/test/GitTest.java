1c267d4121c0ae56d9470b5063be26fc820d84e6
Zjw <2540078388@qq.com>
2022-10-30 22:05:13 +0800

    pro-git master测试

--- src/test/java/com/atguigu/test/GitTest.java
+++ src/test/java/com/atguigu/test/GitTest.java
@@ -4,8 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("提交文件1111");
         System.out.println("im master");
-        System.out.println("im hotfix");
-        System.out.println("im hotfix");
         System.out.println("hello lisi");
+        System.out.println("这里是pro-git的master");
     }
 }
