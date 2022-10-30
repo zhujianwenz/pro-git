99df6df7a990037c875e6131daa373a89e93a4d1
Zjw <2540078388@qq.com>
2022-10-30 17:49:04 +0800

    master合并冲突1

--- src/test/java/com/atguigu/test/GitTest.java
+++ src/test/java/com/atguigu/test/GitTest.java
@@ -3,6 +3,8 @@ package com.atguigu.test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("提交文件1111");
+        System.out.println("im master");
+        System.out.println("im hotfix");
         System.out.println("im hotfix");
     }
 }
