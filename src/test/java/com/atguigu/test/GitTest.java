2136fad8b5e5d4f1f5d0b7fa72ac1f451111c37a
Zjw <2540078388@qq.com>
2022-10-30 17:43:00 +0800

    hotfix测试冲突

--- src/test/java/com/atguigu/test/GitTest.java
+++ src/test/java/com/atguigu/test/GitTest.java
@@ -3,5 +3,6 @@ package com.atguigu.test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("提交文件1111");
+        System.out.println("hotfix在这里");
     }
 }
